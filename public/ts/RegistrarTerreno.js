7026aa48509d52e76ea5a76a97501e9837c9e27f
oguzmarc14 <oguzmarc14@gmail.com>
2025-08-02 22:12:26 -0600

    agregada la key

--- public/ts/RegistrarTerreno.js
+++ public/ts/RegistrarTerreno.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 
 const SUPABASE_URL = "https://ssrmztcxoijibjntrtqe.supabase.co";
-const SUPABASE_KEY = "tu_key";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzcm16dGN4b2lqaWJqbnRydHFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzOTA3NDQsImV4cCI6MjA2ODk2Njc0NH0.Wg9rToI2VzpKrnNmAvRVIlky7bSRjCDfFZ4OuZIaesI";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
