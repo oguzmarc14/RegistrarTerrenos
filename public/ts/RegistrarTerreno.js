4a7866b915e979f0e0b6f2d25ad14ca1276af13e
oguzmarc14 <oguzmarc14@gmail.com>
2026-06-23 19:13:01 -0600

    fix: error al cargar terrenos

--- public/ts/RegistrarTerreno.js
+++ public/ts/RegistrarTerreno.js
@@ -1,5 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
-
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 const SUPABASE_URL = "https://ssrmztcxoijibjntrtqe.supabase.co";
 const SUPABASE_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzcm16dGN4b2lqaWJqbnRydHFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzOTA3NDQsImV4cCI6MjA2ODk2Njc0NH0.Wg9rToI2VzpKrnNmAvRVIlky7bSRjCDfFZ4OuZIaesI";
